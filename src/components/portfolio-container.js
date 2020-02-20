137262565f60784c90a847b3424f4ec4c7482e19
danzjamz <djl.hovley@gmail.com>
2020-02-20 16:28:38 -0700

    added portfolio-container file

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <h1>Done been built</h1>
+            </div>
+        );
+    }
+}
