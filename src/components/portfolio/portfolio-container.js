938a5dd1175d786393a45282bae4288d5eeff0bf
danzjamz <djl.hovley@gmail.com>
2020-02-21 10:08:34 -0700

    created a functional component portfolio-item

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PortfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
+    // for State and lifecycle hooks use class compenent
     render() {
         return (
             <div className='app'>
                 <h1>Done been built</h1>
+                <PortfolioItem />
             </div>
         );
     }
---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+
+export default function PortfolioItem() {
+    // use a functional/presentational component just to render data
+    return (
+        <div>
+            <h3>Portfolio Item</h3>
+        </div>
+    )
+}
