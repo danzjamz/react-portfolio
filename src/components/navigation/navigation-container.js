2a55545fcb2d3b68d31e87a232abb12fa8704b72
danzjamz <djl.hovley@gmail.com>
2020-02-24 16:14:19 -0700

    linked nav to navbar links

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 
 export default class NavigationComponent extends Component {
@@ -11,9 +11,9 @@ export default class NavigationComponent extends Component {
 
     render() {
         return (
-            <div>
-                <button>Home</button>
-                <button>About</button>
+            <div> exact to='/'
+                <NavLink exact to='/'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
                 <button>Contact</button>
                 <button>Blog</button>
                 { false ? <button>Add Blog</button> : null }
