889f7d1426866f1eb9ab6b31aa3c685d1896ee0d
danzjamz <djl.hovley@gmail.com>
2020-02-26 11:20:11 -0700

    added flex styling to nav

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -11,13 +11,19 @@ export default class NavigationComponent extends Component {
 
     render() {
         return (
-            <div> 
-                {/* navlinks add the active class to the current page link */}
-                <NavLink exact to='/'>Home</NavLink> 
-                <NavLink to='/about'>About</NavLink>
-                <NavLink to='/contact'>Contact</NavLink>
-                <NavLink to='/blog'>Blog</NavLink>
-                { false ? <button>Add Blog</button> : null }
+            <div className="nav-wrapper"> 
+                <div className="left-side">
+                    {/* navlinks add the active class to the current page link */}
+                    <NavLink exact to='/'>Home</NavLink> 
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/contact'>Contact</NavLink>
+                    <NavLink to='/blog'>Blog</NavLink>
+                    { false ? <button>Add Blog</button> : null }
+                </div>
+
+                <div className="right-side">
+                    DANZ
+                </div>
             </div>
         )
     }
