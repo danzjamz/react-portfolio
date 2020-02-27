9f886803adcb2fd9499fb3e348cade720a2a9f6b
danzjamz <djl.hovley@gmail.com>
2020-02-27 13:27:38 -0700

    added event handlers for form data

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-
+            email: '',
+            password: ''
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value })
     }
 
     render() {
         return (
             <div>
                 <h1>Login to access your dashboard</h1>
-                <form>
-                    <input type="text"></input>
-                    <input type="password"></input>
+                <form onSubmit={ this.handleSubmit }>
+                    <input type="email" 
+                        name="email" 
+                        placeholder="Email" 
+                        value={ this.state.email } 
+                        onChange={ this.handleChange } />
+                    <input type="password" 
+                        name="password" 
+                        placeholder="Password" 
+                        value={ this.state.password } 
+                        onChange={ this.handleChange } />
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         );
