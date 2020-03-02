e4ea504afe2478a108b83e4967e22bf21df3d885
danzjamz <djl.hovley@gmail.com>
2020-03-02 10:26:38 -0700

    styled images and headings for portfolio-sidebar

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -3,11 +3,11 @@ import React from 'react';
 const PortfolioSidebarList = (props) => {
     const portfolioList = props.data.map(item => {
         return (
-            <div>
-                <div>
+            <div className='portfolio-item-thumb'>
+                <div className='portfolio-thumb-img'>
                     <img src={ item.thumb_image_url } />
                 </div>
-                <h1>{ item.name }</h1>
+                <h1 className='title'>{ item.name }</h1>
                 <h2>{ item.id }</h2>
             </div>
         )
@@ -15,7 +15,7 @@ const PortfolioSidebarList = (props) => {
     console.log(portfolioList)
     
     return (
-        <div>
+        <div className='portfolio-sidebar-list-wrapper'>
             { portfolioList }
         </div>
     )
