ba7fdd5f6498da48bf33b505b6c8a82345e802b1
danzjamz <djl.hovley@gmail.com>
2020-03-05 16:09:39 -0700

    got blog data from api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class Blog extends Component {
     constructor() {
         super();
+
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
     }
-    
+
+    componentDidMount() {
+        this.getBlogItems();
+    }
+
+    getBlogItems() {
+        axios.get('https://danzjamz.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true })
+            .then(response => {
+                this.setState({
+                    blogItems: response.data.portfolio_blogs
+                })
+            }).catch(error => {
+                console.log('getBlogItems error', error);
+            })
+    }
+
     render() {
         return (
             <div>
