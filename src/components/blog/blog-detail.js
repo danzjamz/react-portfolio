2ff83fdb59c4476823ae1d50551a93b75c8b904c
danzjamz <djl.hovley@gmail.com>
2020-03-06 10:27:27 -0700

    got blog item from api in blog-detail

--- src/components/blog/blog-detail.js
+++ src/components/blog/blog-detail.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BlogDetail extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentId: this.props.match.params.slug,
+            blogItem: { }
+        }
+    }
+
+    componentDidMount() {
+        this.getBlogItem();
+    }
+
+    getBlogItem() {
+        axios.get(`https://danzjamz.devcamp.space/portfolio/portfolio_blogs/${ this.state.currentId }`)
+            .then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log('getBlogItem error', error)
+            })
     }
 
     render() {
         return (
             <div>
-                hi
+                { this.state.currentId }
             </div>
         )
     }
