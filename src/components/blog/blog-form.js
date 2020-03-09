eb291ced533f6a78c266689de1c786c37b62e387
danzjamz <djl.hovley@gmail.com>
2020-03-09 15:14:39 -0600

    image uploader submits and clears field

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -21,6 +21,8 @@ export default class BlogForm extends Component {
         this.componentConfig = this.componentConfig.bind(this);
         this.djsConfig = this.djsConfig.bind(this); 
         this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+        this.featuredImageRef = React.createRef();
     }
 
     componentConfig() {
@@ -54,6 +56,10 @@ export default class BlogForm extends Component {
         formData.append('portfolio_blog[title]', this.state.title);
         formData.append('portfolio_blog[blog_status]', this.state.blog_status);
         formData.append('portfolio_blog[content]', this.state.content);
+        
+        if (this.state.featured_image) {
+            formData.append('portfolio_blog[featured_image]', this.state.featured_image);
+        }
 
         return formData;
     }
@@ -70,10 +76,17 @@ export default class BlogForm extends Component {
             { withCredentials: true }
           )
           .then(response => {
+            if (this.state.featured_image) {
+                this.featuredImageRef.current.dropzone.removeAllFiles();
+            }
+                
             this.setState({
-                  title: '',
-                  blog_status: ''
+                    title: '',
+                    blog_status: '', 
+                    content: '',
+                    featured_image: ''
             });
+                
             this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
           })
           .catch(error => {
@@ -103,6 +116,7 @@ export default class BlogForm extends Component {
 
                 <div className='image-uploaders'>
                     <DropzoneComponent
+                        ref={ this.featuredImageRef }
                         config={ this.componentConfig() }
                         djsConfig={ this.djsConfig() }
                         eventHandlers={ this.handleFeaturedImageDrop() }
