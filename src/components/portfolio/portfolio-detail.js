bc6bf70a7cb22a6904874b6ef078f2563ffe1f2b
danzjamz <djl.hovley@gmail.com>
2020-03-10 13:30:21 -0600

    styles for portfolio detail page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -5,7 +5,9 @@ export default class PortfolioDetail extends Component {
     constructor(props) {
         super(props);
 
-        console.log(this.props)
+        this.state = {
+            portfolioItem: {}
+        }
     }
 
     componentWillMount() {
@@ -16,16 +18,51 @@ export default class PortfolioDetail extends Component {
         axios.get(`https://danzjamz.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
             { withCredentials: true })
             .then(response => {
-                console.log(response)
+                this.setState({
+                    portfolioItem: response.data.portfolio_item
+                  });
             }).catch(error => {
                 console.log('get portfolio item error ->', error)
             });
     }
 
     render() {
+        console.log(this.state.portfolio_item)
+        const {
+            banner_image_url,
+            category,
+            description,
+            logo_url,
+            name,
+            thumb_image_url,
+            url
+          } = this.state.portfolioItem;
+
+          const bannerStyles = {
+              backgroundImage: 'url(' + banner_image_url + ')',
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center'
+          }
+          const logoStyles = {
+              width: '200px'
+          }
+      
         return (
-            <div>
-                <h2>Portfolio detail for {this.props.match.params.slug}</h2>
+            <div className='portfolio-detail-wrapper'>
+                <div className='banner' style={ bannerStyles }>
+                    <img src={logo_url} style={ logoStyles } />
+                </div>
+
+                <div className='portfolio-detail-description-wrapper'>
+                    <div className='description'>{ description }</div>
+                </div>
+
+                <div className='bottom-content-wrapper'>
+                    <a href={ url } className='site-link' target='_blank'>
+                        Visit { name }
+                    </a>
+                </div>
             </div>
         )
     }
